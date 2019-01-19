39159eab360921b91112e56f21b542a4b9fc2486
chang <drcxd@sina.com>
2019-01-19 01:43:34 -0800

    add decode function

--- libpngdec.c
+++ libpngdec.c
@@ -4,6 +4,8 @@
 
 #include <png.h>
 
+#define MASK 0x00000001
+
 int main(int argc, char *argv[])
 {
     char buffer[1024];
@@ -84,11 +86,83 @@ int main(int argc, char *argv[])
         row_pointers[row] = NULL;
     for (unsigned row = 0; row < height; ++row)
         row_pointers[row] = png_malloc(png_ptr, png_get_rowbytes(png_ptr, info_ptr));
-    printf("bytes per row %u\n", png_get_rowbytes(png_ptr, info_ptr));
+    printf("bytes per row %zu\n", png_get_rowbytes(png_ptr, info_ptr));
 
     for (unsigned y = 0; y < height; ++y)
         png_read_rows(png_ptr, &row_pointers[y], NULL, 1);
     printf("read finished\n");
+    fclose(fp);
+
+    /* do transform here */
+    int count = 0;
+    for (unsigned i = 0; i < height; ++i)
+    {
+        for (unsigned j = 0; j < width; ++j)
+        {
+            png_bytep pixelp = row_pointers[i] + j * 4;
+            int *pixel = (int *)pixelp;
+            if (*pixel & MASK)
+            {
+                *pixel = 0xffffffff;
+            }
+            else
+            {
+                *pixel = 0xff000000;
+            }
+            /* printf("0x%x\n", *pixel); */
+            ++count;
+        }
+    }
+    printf("total pixle %d\n", count);
+    
+    
+
+    fp = fopen("decode.png", "w");
+    if (!fp)
+    {
+        fprintf(stderr, "opening output file...\n");
+        exit(1);
+    }
+
+    png_structp write_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!write_ptr)
+    {
+        fclose(fp);
+        fprintf(stderr, "creating write structure...\n");
+        exit(1);
+    }
+
+    png_infop write_info = png_create_info_struct(write_ptr);
+    if (!write_info)
+    {
+        fclose(fp);
+        png_destroy_write_struct(&write_ptr, NULL);
+        fprintf(stderr, "creating write info structure...\n");
+        exit(1);
+    }
+
+    if (setjmp(png_jmpbuf(write_ptr)))
+    {
+        fclose(fp);
+        png_destroy_write_struct(&write_ptr, &write_info);
+        fprintf(stderr, "something is wrong...\n");
+        exit(1);
+    }
+
+    png_init_io(write_ptr, fp);
+
+    png_set_IHDR(write_ptr, write_info, width, height, bit_depth,
+        color_type, interlace_type, compression_type, filter_method);
+
+    png_write_info(write_ptr, write_info);
+
+    png_write_image(write_ptr, row_pointers);
+
+    png_write_end(write_ptr, write_info);
+
+    png_destroy_write_struct(&write_ptr, &info_ptr);
+
+    fclose(fp);
     
     return 0;
 }
